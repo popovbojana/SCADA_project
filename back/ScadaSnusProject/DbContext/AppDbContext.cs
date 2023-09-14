5983530d49ba92f2b3dc818f08ebba88f9fb5929
Vuksan Filip <filipvuksan.iphone@gmail.com>
2023-09-14 12:11:39 +0200

    Implemented all DbContext

--- back/ScadaSnusProject/DbContext/AppDbContext.cs
+++ back/ScadaSnusProject/DbContext/AppDbContext.cs
@@ -1,13 +1,22 @@
 using Microsoft.EntityFrameworkCore;
+using SCADA_Project.Model;
 using ScadaSnusProject.Model;
 
 namespace ScadaSnusProject.DbContext;
 
 public class AppDbContext : Microsoft.EntityFrameworkCore.DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-    {
-    }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<User> Users { get; set; }
+    public DbSet<IBaseEntity> IBaseEntities { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<Alarm> Alarms { get; set; }
+    public DbSet<TagRecord> TagRecords { get; set; }
+    public DbSet<AlarmRecord> AlarmRecords { get; set; }
+    public DbSet<DigitalInput> DigitalInputs { get; set; }
+    public DbSet<DigitalOutput> DigitalOutputs { get; set; }
+    public DbSet<AnalogInput> AnalogInputs { get; set; }
+    public DbSet<AnalogOutput> AnalogOutputs { get; set; }
+    
 }
