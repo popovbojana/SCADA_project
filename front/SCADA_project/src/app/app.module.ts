b48d05741a3537fe061e05905795d94d53c7f4b8
popovbojana <popov.bojana@gmail.com>
2023-09-17 20:19:35 +0200

    Added some basic components

--- front/SCADA_project/src/app/app.module.ts
+++ front/SCADA_project/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { TrendingComponent } from './components/trending/trending.component';
+import { TagsComponent } from './components/tags/tags.component';
+import { AlarmsComponent } from './components/alarms/alarms.component';
+import { ReportsComponent } from './components/reports/reports.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegistrationComponent,
+    TrendingComponent,
+    TagsComponent,
+    AlarmsComponent,
+    ReportsComponent
   ],
   imports: [
     BrowserModule,
