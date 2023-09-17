aceaf0bd4615bb27c2bde933f3753fe4941ccae7
popovbojana <popov.bojana@gmail.com>
2023-09-17 20:26:04 +0200

    Added basic routing

--- front/SCADA_project/src/app/app-routing.module.ts
+++ front/SCADA_project/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TrendingComponent } from './components/trending/trending.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { TagsComponent } from './components/tags/tags.component';
+import { AlarmsComponent } from './components/alarms/alarms.component';
+import { ReportsComponent } from './components/reports/reports.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: TrendingComponent},
+  {path: "login", component: LoginComponent},
+  {path: "trending", component: TrendingComponent},
+  {path: "registration", component: RegistrationComponent},
+  {path: "tags", component: TagsComponent},
+  {path: "alarms", component: AlarmsComponent},
+  {path: "reports", component: ReportsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
