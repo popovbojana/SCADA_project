ed39a496c8f62c46282d65556e4ccdc664240d8d
popovbojana <popov.bojana@gmail.com>
2023-09-18 16:11:42 +0200

    Implemented alarms component

--- front/SCADA_project/src/app/services/alarm/alarm.service.ts
+++ front/SCADA_project/src/app/services/alarm/alarm.service.ts
@@ -24,9 +24,9 @@ export class AlarmService {
 }
 
 export interface Alarm{
-  Id?: number,
-  Value: number,
-  TagId: number,
-  Type: string,
-  Priority: string
+  id?: number,
+  value: number,
+  tagId: number,
+  type: string,
+  priority: string
 }
